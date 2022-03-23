a7d29b37678f106449ae9caed0fe1a25529635aa
Tyler Long <tyler4long@gmail.com>
2022-03-23 16:51:23 -0700

    Use official ruleset

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import {Spectral, Document} from '@stoplight/spectral-core';
+import {Spectral, Document, Ruleset} from '@stoplight/spectral-core';
 import {Yaml} from '@stoplight/spectral-parsers'; // make sure to install the package if you intend to use default parsers!
-import {truthy} from '@stoplight/spectral-functions'; // this has to be installed as well
+import {oas} from '@stoplight/spectral-rulesets';
 
 const myDocument = new Document(
   `---
@@ -12,18 +12,7 @@ responses:
 );
 
 const spectral = new Spectral();
-spectral.setRuleset({
-  // a ruleset has to be provided
-  rules: {
-    'no-empty-description': {
-      given: '$..description',
-      message: 'Description must not be empty',
-      then: {
-        function: truthy,
-      },
-    },
-  },
-});
+spectral.setRuleset(oas as unknown as Ruleset);
 
 const main = async () => {
   const result = await spectral.run(myDocument);
