0e6a89b22b64d59039dabf60f6f7cef979c85112
Tyler Long <tyler4long@gmail.com>
2022-04-04 16:45:43 -0700

    First working version of custom ruleset

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This is a demo project to show how to use Spectral programmatically. 
 
 It tries to lint a yaml text with rulesets from `@stoplight/spectral-rulesets`.
+
+
+## Known issues
+
+- https://github.com/microsoft/TypeScript/issues/33079
+  - workaround: delete exports from package.json and `import {xxx} from @stoplight/spectral-ruleset-bundler/dist/loader/node`
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import {Spectral, Document, Ruleset} from '@stoplight/spectral-core';
 import {Yaml} from '@stoplight/spectral-parsers';
-import {oas, asyncapi} from '@stoplight/spectral-rulesets';
 import {readFileSync} from 'fs';
 import {join} from 'path';
-import {load} from 'js-yaml';
+import {bundleAndLoadRuleset} from '@stoplight/spectral-ruleset-bundler/dist/loader/node';
+import * as fs from 'fs';
 
 const myDocument = new Document(
   readFileSync(join(__dirname, 'spec.yml'), 'utf-8').trim(),
@@ -11,18 +11,16 @@ const myDocument = new Document(
   'spec.yml'
 );
 
-const customRules = load(
-  readFileSync(join(__dirname, '.spectral.yml'), 'utf-8').trim()
-) as any;
-
 const spectral = new Spectral();
-spectral.setRuleset({
-  ...asyncapi,
-  ...oas,
-  ...customRules,
-} as unknown as Ruleset);
-
 const main = async () => {
+  const customRules = await bundleAndLoadRuleset(
+    join(__dirname, '.spectral.yml'),
+    {
+      fs,
+      fetch: {} as any,
+    }
+  );
+  spectral.setRuleset(customRules as unknown as Ruleset);
   const issues = await spectral.run(myDocument);
   console.log(JSON.stringify(issues, null, 2));
 };
