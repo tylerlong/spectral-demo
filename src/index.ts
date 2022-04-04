ead9381f6ae531a044fe440522f082eaee2efd8f
Tyler Long <tyler4long@gmail.com>
2022-04-04 14:26:18 -0700

    First try of customer rules (failed)

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {Yaml} from '@stoplight/spectral-parsers';
 import {oas, asyncapi} from '@stoplight/spectral-rulesets';
 import {readFileSync} from 'fs';
 import {join} from 'path';
+import {load} from 'js-yaml';
 
 const myDocument = new Document(
   readFileSync(join(__dirname, 'spec.yml'), 'utf-8').trim(),
@@ -10,8 +11,16 @@ const myDocument = new Document(
   'spec.yml'
 );
 
+const customRules = load(
+  readFileSync(join(__dirname, '.spectral.yml'), 'utf-8').trim()
+) as any;
+
 const spectral = new Spectral();
-spectral.setRuleset({...asyncapi, ...oas} as unknown as Ruleset);
+spectral.setRuleset({
+  ...asyncapi,
+  ...oas,
+  ...customRules,
+} as unknown as Ruleset);
 
 const main = async () => {
   const issues = await spectral.run(myDocument);
